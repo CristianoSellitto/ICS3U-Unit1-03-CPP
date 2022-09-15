59aa9a462cdcb47dd727983215bde390723dbc8e
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-09-15 15:03:00 +0000

    Added program

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+// Copyright Cristiano 2022
+//
+// Created by Cristiano Sellitto
+// Created in September 2022
+// A basic Hello, World !program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.";
+}
